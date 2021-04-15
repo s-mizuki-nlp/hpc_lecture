2680caf514594ec1b7296f67062399a45308b038
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-04-15 11:59:00 +0900

    feat: parallelized pi computation.

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,13 +1,14 @@
 #include <cstdio>
 
 int main() {
-  int n = 10;
+  int n = 100;
   double dx = 1. / n;
   double pi = 0;
+
 #pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
   }
-  printf("%17.15f\n",pi);
+  printf("iteration:%d, %17.15f\n",n, pi);
 }
